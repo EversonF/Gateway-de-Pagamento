380bb1408969dc7055a27076c4881ffb54bba901
EversonF <eversonaraujo589gmail.com>
2025-04-08 20:36:19 -0300

    feat: Enhance account model with API key generation and balance management

--- GO-GATEWAY-API/internal/domain/account.go
+++ GO-GATEWAY-API/internal/domain/account.go
@@ -1,7 +1,10 @@
 package domain
 
 import ("time"
-import "github.com/google/uuid")
+import "github.com/google/uuid"
+import "crypto/rand"
+import "encoding/hex"
+import "sync")
 
 type Account struct {
 	ID string
@@ -9,13 +12,35 @@ type Account struct {
 	Email string
 	APIKey string
 	Balance float64
+	mu sync.RWMutex
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func generateAPIKey() string {
+	b := make([]byte, 16)
+	rand.Read(b)
+	return hex.EncodeToString(b)
 }
 
 func NewAccount(name, email string) *Account {
 	
 	account := &Account{
 		ID: uuid.New().String(),
+		Name: name,
+		Email: email,
+		Balance: 0.0,
+		APIKey: generateAPIKey(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
 	}
+	return account
+}
+
+func (a *Account) AddBalance(amount float64){
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	a.Balance += amount
+	a.UpdatedAt = time.Now()
+	a.mu.Unlock()
 }
