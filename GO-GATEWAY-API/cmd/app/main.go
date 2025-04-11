0d58c366e7475824a81ce2d72d2e680adc88ad00
EversonF <eversonaraujo589gmail.com>
2025-04-10 21:21:06 -0300

    feat: Set up server and database connection with environment configuration

--- GO-GATEWAY-API/cmd/app/main.go
+++ GO-GATEWAY-API/cmd/app/main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"log"
+	"fmt"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+func getEnv(key string, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
+
 func main ()  {
-	
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		getEnv("DB_HOST", "db"),
+		getEnv("DB_PORT", "5432"),
+		getEnv("DB_USER", "postgres"),
+		getEnv("DB_PASSWORD", "postgres"),
+		getEnv("DB_NAME", "gateway"),
+		getEnv("DB_SSLMODE", "disable"),
+
+	)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Error connecting to the database: ", err)
+	}
+	defer db.Close()
+
+	accountRepository := repository.NewAccountRepository(db)
+	accountService := service.NewAccountService(accountRepository)
+
+	port := getEnv("PORT", "8080")
+	server := server.NewServer(accountService, port)
+	server.ConfigureRoutes()
+
+	server.Start(); if err != nil {
+		log.Fatalf("Error starting server: ", err)
+	}
 }
